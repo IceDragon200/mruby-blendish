5f4f0b8bd4caffa51ca8d252925a43266dee5f2a
Corey Powell <mistdragon100@gmail.com>
2017-01-04 17:44:53 -0500

    Made extern optional

--- src/bnd_common.h
+++ src/bnd_common.h
@@ -13,7 +13,11 @@
 #define BLENDISH_NO_NVG_TYPEDEFS
 #include <blendish.h>
 
+#ifdef __cplusplus
 #define MRB_BND_EXTERN extern
+#else
+#define MRB_BND_EXTERN
+#endif
 
 MRB_BND_EXTERN mrb_value mrb_nvg_color_value(mrb_state*, NVGcolor);
 MRB_BND_EXTERN const struct mrb_data_type mrb_nvg_color_type;
